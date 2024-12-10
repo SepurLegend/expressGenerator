d1a3f4b947d6c3f754af9dc742d7694fe5da8b13
SepurLegend <161539066+SepurLegend@users.noreply.github.com>
2024-12-10 12:42:12 +0700

    Comment role dan auth middleware pada prodi

--- app_api/routes/prodi.js
+++ app_api/routes/prodi.js
@@ -12,28 +12,28 @@ const roleMiddleware = require("../middleware/roleMiddleware");
 // Definisi rute untuk prodi
 // Mengatur rute GET untuk mendapatkan semua data prodi
 router.get("/", 
-    authMiddleware,
-    roleMiddleware("admin"),
+    // authMiddleware,
+    // roleMiddleware("admin"),
     prodiController.getAllProdi);
 // Mengatur rute POST untuk membuat data prodi baru
 router.post("/", 
-    authMiddleware,
-    roleMiddleware("admin"),
+    // authMiddleware,
+    // roleMiddleware("admin"),
     prodiController.createProdi);
 // Mengatur rute GET untuk mendapatkan data prodi berdasarkan ID
 router.get("/:id", 
-    authMiddleware,
-    roleMiddleware("admin"),
+    // authMiddleware,
+    // roleMiddleware("admin"),
     prodiController.getProdiById);
 // Mengatur rute PUT untuk memperbarui data prodi berdasarkan ID
 router.put("/:id", 
-    authMiddleware,
-    roleMiddleware("admin"),
+    // authMiddleware,
+    // roleMiddleware("admin"),
     prodiController.updateProdi);
 // Mengatur rute DELETE untuk menghapus data prodi berdasarkan ID
 router.delete("/:id", 
-    authMiddleware,
-    roleMiddleware("admin"),
+    // authMiddleware,
+    // roleMiddleware("admin"),
     prodiController.deleteProdi);
 
 // Mengeksport router agar dapat digunakan di file lain (misalnya, di app.js)
